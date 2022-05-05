f7c73002d8c9f1df7139bc4ad13c3096634408c1
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-05 15:50:25 +0530

    Update utils.py

--- spam-ham-web-app-master/utils.py
+++ spam-ham-web-app-master/utils.py
@@ -2,7 +2,7 @@ import string
 from nltk.corpus import stopwords
  
       
-def text_process(message):   
+def text_process(message):    
     noPunc = [char for char in message if char not in string.punctuation]      
     noPunc = ''.join(noPunc)  
  
