da72915c9f9b9ac99401b704cae75f54098597f9
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-05 15:47:06 +0530

    Update utils.py

--- spam-ham-web-app-master/utils.py
+++ spam-ham-web-app-master/utils.py
@@ -4,6 +4,6 @@ from nltk.corpus import stopwords
  
 def text_process(message):  
     noPunc = [char for char in message if char not in string.punctuation] 
-    noPunc = ''.join(noPunc)
+    noPunc = ''.join(noPunc) 
 
     return [word for word in noPunc.split() if word not in stopwords.words('english')]
